2dffd30c83644e995f64c81dfada935cef6a6fa4
Nick Johnstone <ncwjohnstone@gmail.com>
2013-09-12 22:32:47 +1200

    Moved to separate processes

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from webui import Core
-from threading import Thread
+from multiprocessing import Process
 
 import os
 
@@ -8,15 +8,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-	return os.listdir('.')
+  return os.listdir('.')
 
 def run_web_app():
-	app.run()
+  app.run()
 
 if __name__ == '__main__':
-	core = Core("http://127.0.0.1:5000")
-	thread = Thread(target=run_web_app)
-	thread.start()
-	core.run()
-	thread.join()
-	sys.exit()
+  core = Core("http://127.0.0.1:5000")
+  flask_proc = Process(target=run_web_app)
+  flask_proc.start()
+  core.run()
+  flask_proc.join()
+  sys.exit()
