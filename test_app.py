24e41b1382e25eceb254c58f107af81adfc1e882
Nick Johnstone <ncwjohnstone@gmail.com>
2013-09-13 00:09:11 +1200

    Moved back to hello world for time being

--- test_app.py
+++ test_app.py
@@ -7,11 +7,9 @@ import os
 app = Flask(__name__)
 ui = WebUI(app, debug=True)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def main(path):
-  file_path = "C:"+request.path
-  return render_template("basic.html", url=request.path, items= [".."] + os.listdir(file_path))
+@app.route('/')
+def main():
+  return "Hello world!"
 
 if __name__ == '__main__':
   ui.run()
