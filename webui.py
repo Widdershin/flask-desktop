9f81c9cb18de0b056b25f867b5f034a922cb22ef
Widdershin <ncwjohnstone@gmail.com>
2013-09-13 15:42:14 +1200

    added license actually and changed from tabs to spaces

--- webui.py
+++ webui.py
@@ -5,27 +5,27 @@ import PySide.QtWebKit as web_core
 import PySide.QtGui as gui_core
 
 class WebUI(object):
-	def __init__(self, app, url="http://127.0.0.1:5000", debug=False):
-		self.flask_app = app
-		self.flask_thread = Thread(target=self._run_flask, args=(debug,))
-		self.flask_thread.daemon = True;
+  def __init__(self, app, url="http://127.0.0.1:5000", debug=False):
+    self.flask_app = app
+    self.flask_thread = Thread(target=self._run_flask, args=(debug,))
+    self.flask_thread.daemon = True;
 
-		self.url = url
-		self.app = gui_core.QApplication([])
-		self.view =  web_core.QWebView(self.app.activeModalWidget())
+    self.url = url
+    self.app = gui_core.QApplication([])
+    self.view =  web_core.QWebView(self.app.activeModalWidget())
 
-	def run(self):
-		self.run_flask()
-		self.run_gui()
+  def run(self):
+    self.run_flask()
+    self.run_gui()
 
-		 
-	def run_flask(self):
-		self.flask_thread.start()
+     
+  def run_flask(self):
+    self.flask_thread.start()
 
-	def run_gui(self):
-		self.view.load(qt_core.QUrl(self.url))
-		self.view.show()
-		self.app.exec_()
+  def run_gui(self):
+    self.view.load(qt_core.QUrl(self.url))
+    self.view.show()
+    self.app.exec_()
  
-	def _run_flask(self, debug):
-		self.flask_app.run(debug=debug, use_reloader=False)
+  def _run_flask(self, debug):
+    self.flask_app.run(debug=debug, use_reloader=False)
