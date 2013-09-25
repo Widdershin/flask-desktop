c5d18ddab778b8a4de7610425cb13a277111b77d
Nick Johnstone <ncwjohnstone@gmail.com>
2013-09-25 23:51:37 +1200

    Added inspector when using debug, and enabled flash and local storage by default

--- webui/webui.py
+++ webui/webui.py
@@ -5,30 +5,44 @@ import PySide.QtWebKit as web_core
 import PySide.QtGui as gui_core
 
 class WebUI(object):
-  def __init__(self, app, url="http://127.0.0.1:5000", debug=False, using_win32=False):
-    self.flask_app = app
-    self.flask_thread = Thread(target=self._run_flask, args=(debug, using_win32,))
-    self.flask_thread.daemon = True;
+    def __init__(self, app, url="http://127.0.0.1:5000", debug=False, using_win32=False):
+        self.flask_app = app
+        self.flask_thread = Thread(target=self._run_flask, args=(debug, using_win32,))
+        self.flask_thread.daemon = True;
+        self.debug = debug
 
-    self.url = url
-    self.app = gui_core.QApplication([])
-    self.view = web_core.QWebView(self.app.activeModalWidget())
+        self.url = url
+        self.app = gui_core.QApplication([])
+        self.view = web_core.QWebView(self.app.activeModalWidget())
 
-  def run(self):
-    self.run_flask()
-    self.run_gui()
+    def run(self):
+        self.run_flask()
+        self.run_gui(inspector=self.debug)
 
-     
-  def run_flask(self):
-    self.flask_thread.start()
+    def run_flask(self):
+        self.flask_thread.start()
 
-  def run_gui(self):
-    self.view.load(qt_core.QUrl(self.url))
-    self.view.show()
-    self.app.exec_()
- 
-  def _run_flask(self, debug=False, using_win32=False):
-    if using_win32:
-      import pythoncom
-      pythoncom.CoInitialize()
-    self.flask_app.run(debug=debug, use_reloader=False)
+    def run_gui(self, inspector=False):
+        self.view.load(qt_core.QUrl(self.url))
+
+        change_setting = self.view.page().settings().setAttribute
+        change_setting(web_core.QWebSettings.DeveloperExtrasEnabled, True)
+        change_setting(web_core.QWebSettings.LocalStorageEnabled, True)
+        change_setting(web_core.QWebSettings.OfflineStorageDatabaseEnabled, True)
+        change_setting(web_core.QWebSettings.OfflineWebApplicationCacheEnabled, True)
+        change_setting(web_core.QWebSettings.PluginsEnabled, True)
+
+        self.view.show()
+
+        if inspector:
+            self.inspector = web_core.QWebInspector()
+            self.inspector.setPage(self.view.page())
+            self.inspector.show()
+
+        self.app.exec_()
+
+    def _run_flask(self, debug=False, using_win32=False):
+        if using_win32:
+            import pythoncom
+            pythoncom.CoInitialize()
+        self.flask_app.run(debug=debug, use_reloader=False)
