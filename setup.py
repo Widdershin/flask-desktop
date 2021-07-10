a38475bc8144387a318c7eaf4d85ff1b80b78f89
fanoway <86997883+fanoway@users.noreply.github.com>
2021-07-09 20:55:49 -0700

    Revert "Updated dependencies"
    This reverts commit 778be46ab70b2579ff24059138121124ff49fdc1.

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
 from setuptools import setup
 
-with open("requirements.txt") as f:
+with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name="WebUI",
-    version="0.2.1",
-    author="Nick Johnstone",
-    author_email="ncwjohnstone@gmail.com",
-    packages=["webui"],
-    scripts=["examples/test_app.py"],
-    url="https://github.com/Widdershin/WebUI/",
+    name='WebUI',
+    version='0.2.1',
+    author='Nick Johnstone',
+    author_email='ncwjohnstone@gmail.com',
+    packages=['webui'],
+    scripts=['examples/test_app.py'],
+    url='https://github.com/Widdershin/WebUI/',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: MIT License",
         "Operating System :: OS Independent",
     ],
-    description=(
-        "WebUI lets you create first class desktop applications in Python with HTML/CSS"
-    ),
-    long_description=open("README.md").read(),
+    description='WebUI lets you create first class desktop applications in Python with HTML/CSS',
+    long_description=open('README.md').read(),
     install_requires=required,
-    python_requires=">=3.6",
+    python_requires='>=3.6',
 )
